f07e926b63450e8650a782df3bc7b511201980cd
Karlapudi Rakesh <rakeshkarlapudi276@gmail.com>
2025-07-10 16:24:20 +0530

    Fix: corrected import to Root.jsx in main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import Root from './root.jsx'; 
+import Root from './Root.jsx'; 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
