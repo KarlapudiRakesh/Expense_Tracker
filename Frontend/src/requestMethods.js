68062f4b7f3f9c1ca1f38630ff90b75dddd6aaa4
Karlapudi Rakesh <rakeshkarlapudi276@gmail.com>
2025-07-10 19:19:30 +0530

    Fix: Added timeout for Axios to handle Render cold starts

--- Frontend/src/requestMethods.js
+++ Frontend/src/requestMethods.js
@@ -1,15 +1,18 @@
+
 import axios from "axios";
 
-const BASE_URL = "https://expense-tracker-b-mpun.onrender.com/api";
+const BASE_URL = "https://expense-tracker-b-mpun.onrender.com/api"; 
 
 const token = localStorage.getItem("token");
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
+  timeout: 30000, 
 });
 
 export const userRequest = axios.create({
   baseURL: BASE_URL,
+  timeout: 30000,
   headers: {
     token: token,
   },
