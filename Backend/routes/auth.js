2edbb30aa2c3bf7829b54f1c22fc6b94edba27b1
Karlapudi Rakesh <rakeshkarlapudi276@gmail.com>
2025-07-10 15:45:49 +0530

    Fix: updated auth.js

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const User = require("../models/user");
-const bcrypt = require("bcryptjs");
+const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
 router.post("/register", async (req, res) => {
